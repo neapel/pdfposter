d1b0d398c949bf8f2fe3a878d917269b027cc33f
Pascal Germroth <funkycoder@gmail.com>
2009-12-19 12:40:08 +0100

    c

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ packages = poppler-cairo
 CXXFLAGS += `pkg-config --cflags $(packages)` -g3 -I/usr/include/poppler --std=c++0x
 LDLIBS += `pkg-config --libs $(packages)` /usr/local/lib/libpoppler-cairo.a
 
-pdfposter : pdfposter.cc
+pdfposter : pdfposter.cc cmdline.c
 
 
-cmdline.c : options.ggo
+cmdline.c cmdline.h : options.ggo
 	gengetopt -i $< -F $*
--- pdfposter.cc
+++ pdfposter.cc
@@ -9,8 +9,15 @@
 #include <iostream>
 
 
-#include <array>
+#include <vector>
 #include <string>
+#include <boost/algorithm/string.hpp>
+#include <sstream>
+
+#include "cmdline.h"
+
+using namespace std;
+
 
 
 struct rect {
@@ -22,26 +29,6 @@ struct rect {
 
 };
 
-
-std::pair<std::string, rect> paper[] = {
-	{"letter", rect(612, 792)},
-	{"tabloid", rect(792, 1224)},
-	{"ledger", rect(1224, 792)},
-	{"legal", rect(612, 1008)},
-	{"statement", rect(396, 612)},
-	{"executive", rect(540, 720)},
-	{"a0", rect(2384, 3371)},
-	{"a1", rect(1685, 2384)},
-	{"a2", rect(1190, 1684)},
-	{"a3", rect(842, 1190)},
-	{"a4", rect(596, 842)},
-	{"a5", rect(420, 595)},
-	{"b4", rect(729, 1032)},
-	{"b5", rect(516, 729)},
-	{"folio", rect(612, 936)},
-	{"quarto", rect(610, 780)},
-	{"10x14", rect(720, 1008)}
-};
 
 
 
@@ -65,7 +52,7 @@ struct tile : rect {
 
 struct poster {
 	PDFDoc *pdf;
-	std::list<tile> slices;
+	list<tile> slices;
 	rect in_size, bb;
 	int page;
 	double mark_size;
@@ -82,7 +69,7 @@ struct poster {
 		double border = margin + bleed;
 		double box_w = (out_w - 2 * border) / scale, box_h = (out_h - 2 * border) / scale;
 
-		double in_w = in_size.w - 2 * border / scale, in_h = in_size.h - 2 * border / scale;
+		double in_w = in_size.w /*- 2 * border / scale */, in_h = in_size.h /*- 2 * border / scale */;
 
 		// Choose Configuration that wastes less space
 		bool normal = true;
@@ -96,8 +83,8 @@ struct poster {
 
 		// Swap
 		if( !normal ) {
-			std::swap( out_w, out_h );
-			std::swap( box_w, box_h );
+			swap( out_w, out_h );
+			swap( box_w, box_h );
 		}
 
 		// Make tiles
@@ -133,7 +120,7 @@ struct poster {
 
 	void render( cairo_t *cr ) {
 		for( auto I = slices.begin() ; I != slices.end() ; I++ ) {
-			std::cerr << "slice " << I->cut.x << " " << I->cut.y << " " << I->cut.w << " " << I->cut.h << std::endl;
+			cerr << "slice " << I->cut.x << " " << I->cut.y << " " << I->cut.w << " " << I->cut.h << endl;
 			
 			double w = I->paper_w, h = I->paper_h;
 			double m = I->margin, b = I->bleed;
@@ -151,10 +138,10 @@ struct poster {
 			cairo_restore( cr );
 
 			// Cut Marks
-			double bx0 = std::max( bo, std::min( w - bo, bo - I->cut.x * s ) ),
-			       bx1 = std::max( bo, std::min( w - bo, bo + (in_size.w - I->cut.x) * s ) ),
-			       by0 = std::max( bo, std::min( h - bo, bo - I->cut.y * s ) ),
-			       by1 = std::max( bo, std::min( h - bo, bo + (in_size.h - I->cut.y) * s ) );
+			double bx0 = max( bo, min( w - bo, bo - I->cut.x * s ) ),
+			       bx1 = max( bo, min( w - bo, bo + (in_size.w - I->cut.x) * s ) ),
+			       by0 = max( bo, min( h - bo, bo - I->cut.y * s ) ),
+			       by1 = max( bo, min( h - bo, bo + (in_size.h - I->cut.y) * s ) );
 			
 			cairo_save( cr );
 			cairo_translate( cr, bx0, by0 );
@@ -204,7 +191,7 @@ struct poster {
 		cairo_close_path( cr );
 	}
 
-	void render( std::string outname ) {
+	void render( string outname ) {
 		cairo_surface_t *surf = cairo_pdf_surface_create( outname.c_str(), 0, 0 );
 		cairo_t *cr = cairo_create( surf );
 		render( cr );
@@ -226,7 +213,7 @@ struct poster {
 		cairo_restore( cr );
 	}
 
-	void render_preview( std::string outname ) {
+	void render_preview( string outname ) {
 		cairo_surface_t *surf = cairo_pdf_surface_create( outname.c_str(), bb.w, bb.h );
 		cairo_t *cr = cairo_create( surf );
 		render_preview( cr );
@@ -234,7 +221,7 @@ struct poster {
 		cairo_destroy( cr );
 	}
 
-	void render_with_preview( std::string outname ) {
+	void render_with_preview( string outname ) {
 		cairo_surface_t *surf = cairo_pdf_surface_create( outname.c_str(), bb.w, bb.h );
 		cairo_t *cr = cairo_create( surf );
 		render_preview( cr );
@@ -248,10 +235,10 @@ struct poster {
 		rect bb = in_size;
 		for( auto I = slices.begin() ; I != slices.end() ; I++ ) {
 			double b = I->margin + I->bleed;
-			bb.x = std::min( bb.x, I->cut.x - b );
-			bb.y = std::min( bb.y, I->cut.y - b );
-			bb.w = std::max( bb.w, I->cut.x + I->cut.w + b );
-			bb.h = std::max( bb.h, I->cut.y + I->cut.h + b );
+			bb.x = min( bb.x, I->cut.x - b );
+			bb.y = min( bb.y, I->cut.y - b );
+			bb.w = max( bb.w, I->cut.x + I->cut.w + b );
+			bb.h = max( bb.h, I->cut.y + I->cut.h + b );
 		}
 		bb.w -= bb.x;
 		bb.h -= bb.y;
@@ -264,16 +251,112 @@ struct poster {
 
 
 
+double parse_length( string s ) {
+	double l;
+	string e;
+	istringstream(s) >> l >> e;
+
+	cerr << s << " => " << l << ", " << e << endl;
+
+	if( e.size() == 0 || e == "pt" ) return l;
+
+	if( e == "in" ) return l * 72;
+	if( e == "ft" ) return l * 864;
+
+	const double pm = 72 / 25.4;
+	if( e == "mm" ) return l * pm;
+	if( e == "cm" ) return l * (pm * 10);
+	if( e == "m" ) return l * (pm * 1000);
+
+	cerr << "Unknown unit, please use one of pt, in, ft, mm, cm or m" << endl;
+	exit(-4);
+}
+
+
+rect find_paper( string n ) {
+
+	vector< pair<string, rect> > sizes = {
+		{"letter", rect(612, 792)},
+		{"tabloid", rect(792, 1224)},
+		{"ledger", rect(1224, 792)},
+		{"legal", rect(612, 1008)},
+		{"statement", rect(396, 612)},
+		{"executive", rect(540, 720)},
+		{"a0", rect(2384, 3371)},
+		{"a1", rect(1685, 2384)},
+		{"a2", rect(1190, 1684)},
+		{"a3", rect(842, 1190)},
+		{"a4", rect(596, 842)},
+		{"a5", rect(420, 595)},
+		{"b4", rect(729, 1032)},
+		{"b5", rect(516, 729)},
+		{"folio", rect(612, 936)},
+		{"quarto", rect(610, 780)},
+		{"10x14", rect(720, 1008)}
+	};
+
+	for( auto I = sizes.begin() ; I != sizes.end() ; I++ )
+		if( boost::iequals( I->first, n ) ) return I->second;
+
+
+	rect out;
+
+	size_t mid = n.find_first_of("x*,:; ");
+	if( mid == string::npos ) {
+		cerr << "No known paper format, please specify box size as '<length>x<length>'" << endl;
+		exit(-5);
+	}
+
+	out.w = parse_length( n.substr( 0, mid ) );
+	out.h = parse_length( n.substr( mid + 1 ) );
+
+	return out;
+}
+
 
 int main(int argc, char *argv[]) {
 
+	gengetopt_args_info info;
+	if( cmdline_parser( argc, argv, &info ) != 0 )
+		return -1;
+
+	if( info.inputs_num != 2 ) {
+		cerr << "Please specify input- and output filenames." << endl;
+		return -2;
+	}
+
+	string input( info.inputs[0] ), output( info.inputs[1] );
+
+	// Open PDF
 	globalParams = new GlobalParams();
 
-	PDFDoc *doc = new PDFDoc( new GooString(argv[1]), NULL, NULL );
+	PDFDoc *doc = new PDFDoc( new GooString( input.c_str() ), NULL, NULL );
+	if( !doc ) {
+		cerr << "The PDF file <" << input << "> could not be read..." << endl;
+		return -3;
+	}
 
-	poster p( doc );
-	p.slice( 595 * 1, 842, 3, 70, 30 );
-	p.render_with_preview( std::string(argv[2]) );
+	poster my_poster( doc );
+
+
+	// Parse options
+	double bleed = parse_length( string( info.cut_arg ) );
+	double margin = parse_length( string( info.white_arg ) );
+	rect paper = find_paper( string( info.media_arg ) );
+	rect target = my_poster.in_size;
+	if( info.poster_given ) target = find_paper( string( info.poster_arg ) );
+
+	if( target.w == 0 ) target.w = target.h * my_poster.in_size.w / my_poster.in_size.h;
+	else if( target.h == 0 ) target.h = target.w * my_poster.in_size.h / my_poster.in_size.w;
+
+	double scale = min( target.w / my_poster.in_size.w, target.h / my_poster.in_size.h );
+	if( info.scale_given ) scale = info.scale_arg;
+
+	my_poster.slice( paper.w, paper.h, scale, margin, bleed );
+
+	if( info.guide_flag ) my_poster.render_with_preview( output );
+	else my_poster.render( output );
+
 
 	return 0;
 }
